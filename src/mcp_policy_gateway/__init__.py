f54e0923196069533dac542d44d5ed28f6d9f218
AHMED QAID <ahmed0qaid@gmail.com>
2026-08-30 17:47:04 +0300

    Export v0.3 zero-trust APIs

--- src/mcp_policy_gateway/__init__.py
+++ src/mcp_policy_gateway/__init__.py
@@ -3,15 +3,27 @@ from .gateway import AuditEvent, PolicyGateway
 from .policy import Decision, Policy, PolicyEngine, Rule
 from .registry import UpstreamConfig, UpstreamRegistry
 from .schema_pin import SchemaPinStore, SchemaVerification, schema_digest
+from .security import (
+    AgentIdentity,
+    ApprovalTokenSigner,
+    EgressPolicy,
+    SlidingWindowRateLimiter,
+    arguments_digest,
+    prompt_injection_signals,
+    redact_sensitive,
+)
 from .server import PolicyMCPServer
 from .service import PolicyGatewayService
 from .transport import CircuitBreaker, CircuitOpenError, SDKConnector
 
 __all__ = [
+    "AgentIdentity",
+    "ApprovalTokenSigner",
     "AuditEvent",
     "CircuitBreaker",
     "CircuitOpenError",
     "Decision",
+    "EgressPolicy",
     "JsonlAuditSink",
     "NetworkAuditEvent",
     "Policy",
@@ -23,7 +35,11 @@ __all__ = [
     "SDKConnector",
     "SchemaPinStore",
     "SchemaVerification",
+    "SlidingWindowRateLimiter",
     "UpstreamConfig",
     "UpstreamRegistry",
+    "arguments_digest",
+    "prompt_injection_signals",
+    "redact_sensitive",
     "schema_digest",
 ]
